ba8217a1d222eb4bf281dc927c9489bc456280e1
R0bert196 <robertchiriac196@yahoo.com>
2023-03-15 17:13:59 +0200

    Create tournament, update tournament name

--- client-app/src/views/CreateTournament.jsx
+++ client-app/src/views/CreateTournament.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 function CreateTournament() {
   const navigation = useNavigation();
+  const [isEditable, setIsEditable] = useState(false);
+  const [tournamentName, setTournamentName] = useState("Tournament Name")
+
+  const handleEditNamePress = () => {
+    setIsEditable(oldValue => !oldValue);
+  }
 
   return (
     <View style={styles.container}>
+      <View style={styles.tournamentNameWrapper}>
+
+        {!isEditable ?
+          <Text style={styles.tournamentWrapperLeft}>{tournamentName}</Text> : 
+          <TextInput style={styles.textInput} onChangeText={text => setTournamentName(text)}/>
+          }  
+        
+        <TouchableOpacity style={styles.tournamentWrapperRight}
+          onPress={handleEditNamePress}
+        >
+            <Text>{isEditable ? 'Save' : 'Edit'} </Text>
+          </TouchableOpacity> 
+      </View>
+     
+      
       <Text style={styles.text}>Create Tournament</Text>
     </View>
   );
@@ -22,6 +43,27 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  tournamentNameWrapper: {
+    // flex: 1,
+    // justifyContent: 'space-between',
+    // alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10
+    // paddingHorizontal: 10
+  },
+  tournamentWrapperLeft: {
+    // flexDirection: 'row',
+    // alignItems: 'center'
+  },
+  tournamentWrapperRight: {
+    // flexDirection: 'row',
+    // alignItems: 'center'
+  },
+  textInput: {
+    borderWidth: 2,
+    minWidth: 50,
+    maxWidth: 250,
+  }
 });
 
 export default CreateTournament;
