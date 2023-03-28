0d985214e287b92bfe49a268d7a98ec9d71d6e8b
R0bert196 <robertchiriac196@yahoo.com>
2023-03-28 17:45:55 +0300

    Add Tournament model fields for db

--- PronoFuture/Models/Tournament.cs
+++ PronoFuture/Models/Tournament.cs
@@ -1,4 +1,6 @@
-﻿namespace PronoFuture.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PronoFuture.Models
 {
     public class Tournament
     {
@@ -7,6 +9,21 @@
         public DateTime StartingDate { get; set; }
         public DateTime EndingDate { get; set; }
         public EnumTournamentStatus TournamentStatus { get; set; }
+        public EnumChampionshipType ChampionshipType { get; set; }
+        [StringLength(10)]
+        public string AccessKey { get; set; }
+        public bool IsPublic { get; set; }
+        public int SignedPlayers { get; set; }
+        public float TournamentPrice { get; set; }
+        public EnumTournamentPronosticFrequency EnumTournamentPronosticFrequency { get; set; }
+        
+        
+        
+        public int TournamentMasterId { get; set; }
+        public User TournamentMaster { get; set; }
+        
+        
+        
         public int ChampionshipId { get; set; }
         public Championship Championship { get; set; }
         public int UserId { get; set; }
